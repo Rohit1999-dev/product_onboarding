91d18d16d5ef2538c1aeb324848b4c9473c13b43
Rohit Yadav <44015004+rohityo@users.noreply.github.com>
2019-09-18 19:38:34 +0530

    Update orc.js

--- orc.js
+++ orc.js
@@ -1,22 +1,27 @@
-var tesseract = require('node-tesseract-ocr');
-var AWS = require('aws-sdk');
-var express = require('express');
+var tesseract = require('node-tesseract-ocr');		// tesseract is library is used for extracted text from image
+
+var AWS = require('aws-sdk');			// AWS is used for comprehend for data analysis
+var express = require('express');	// express is framework for use of node js web Application
 var app = express();
 
-
+					// Here is Api-Endpoint for data extracted from images.
 app.get("/path",(req, res)=>{
   const config={
     lan:"eng",
     oem:1,
     psm:3
 }
+  			// Here is my amazon accesskey for uses of AWS.
 AWS.config.update({
     accessKeyId: "AKIAJX5MFR26KWUJF65A",
     secretAccessKey: "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",
     region: 'us-east-1'
   });
+				// Here I required comprehend for accesible amazon key.
+
   var comprehend = new AWS.Comprehend();
 
+									// Here I required images of path from files
 tesseract.recognize("/home/rohit/Desktop/orc/product_onboarding/visiting-cards/card1.JPG",config)
 .then((Data)=>{
     var params = {
@@ -28,7 +33,7 @@ tesseract.recognize("/home/rohit/Desktop/orc/product_onboarding/visiting-cards/c
         console.log(err, err.stack); 
       }
       else { 
-        var da_ta = (Data.ResultList[0]);
+        var da_ta = (Data.ResultList[0]);	// Here Analysis of data from Data for image.
         var k = da_ta.Entities
         for (var i of k){
           var data = i["Text"]
@@ -39,6 +44,7 @@ tesseract.recognize("/home/rohit/Desktop/orc/product_onboarding/visiting-cards/c
     });
 });
 })
+	 // Here is my Localhost runnning on this Port
 
 app.listen(5080,()=>{
 	console.log('your app is listening')
